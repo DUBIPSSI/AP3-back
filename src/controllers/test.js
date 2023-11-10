b3a978d45bb37649a45b078b659ab8ae4085082a
Rissip <r.borchani@ecole-ipssi.net>
2023-11-10 12:40:32 +0100

    Maj

--- src/controllers/test.js
+++ src/controllers/test.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const codesPostaux = require('codes-postaux');
 const { ajouterUtilisateur, verifPassword, getCity } = require('../business/add');
 const { getUtilisateurs, getClub, getEvenement } = require('../business/get');
+router.use(express.json());
 
-router.post('/add', (req, res) => {
-  const { username, userprenom, role, birth, codepostale, email, password } = req.body;
-  // const { ville, departement } = getCity(codepostale);
-  const pass = verifPassword(password);
-  ajouterUtilisateur(username, userprenom, role, birth, null, null, email, pass);
-  res.send('Utilisateur ajouté avec succès.'); // Réponse pour la requête POST
+router.post('/add', async (req, res) => {
+  try {
+    console.log(req.body);
+    const { username, userprenom, codepostale, email, password } = req.body;
+    const birth = '1999-01-01';
+    const role = 1;
+    // const pass = await verifPassword(password);
+    // console.log(pass);
+    await ajouterUtilisateur(username, userprenom, role, birth, 'ville', 'departement', email, password);
+    res.send('Utilisateur ajouté avec succès.');
+  } catch (error) {
+    res.status(500).json({ error: "Erreur lors de l'ajout de l'utilisateur." });
+  }
 });
 
 router.get('/utilisateur', async (req, res) => {
@@ -17,13 +26,13 @@ router.get('/utilisateur', async (req, res) => {
     res.json(utilisateurs); // Répondre avec les utilisateurs en format JSON
   } catch (error) {
     res.status(500).json({ error: 'Erreur lors de la récupération des utilisateurs.' });
-  } 
+  }
 });
 
 router.get('/club', async (req, res) => {
   try {
     const utilisateurs = await getClub();
-    res.json(utilisateurs); // Répondre avec les utilisateurs en format JSON
+    res.json(utilisateurs);
   } catch (error) {
     res.status(500).json({ error: 'Erreur lors de la récupération des utilisateurs.' });
   }
@@ -32,10 +41,17 @@ router.get('/club', async (req, res) => {
 router.get('/evenement', async (req, res) => {
   try {
     const utilisateurs = await getEvenement();
-    res.json(utilisateurs); // Répondre avec les utilisateurs en format JSON
+    res.json(utilisateurs);
   } catch (error) {
     res.status(500).json({ error: 'Erreur lors de la récupération des utilisateurs.' });
   }
 });
-
+router.get('/:codepostale', async (req, res) => {
+  try {
+    const departement = await codesPostaux.find(req.params.codepostale);
+    res.json(departement);
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de la récupération des utilisateurs.' });
+  }
+});
 module.exports = router;

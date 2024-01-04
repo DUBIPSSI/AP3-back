9fcf57f382e831f015ba67e474ec31631d346aaa
Rissip <r.borchani@ecole-ipssi.net>
2024-01-04 12:09:30 +0100

    fix bug

--- src/controllers/update.js
+++ src/controllers/update.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/updateAvatar', async (req, res) => {
-  const { avatar } = req.body;
+  const { avatar, email } = req.body;
   try {
-    await updateUtilisateur(avatar);
+    await updateUtilisateur(avatar, email);
     res.send('Utilisateur modifié avec succès.');
   } catch (error) {
     res.status(500).json({ error: "Erreur lors de la modification de l'utilisateur." });

f2e08cd843baac66b5b66a85a32bbc94c0369fc2
Rissip <r.borchani@ecole-ipssi.net>
2024-01-04 12:07:15 +0100

    creat /update

--- src/controllers/update.js
+++ src/controllers/update.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/update', async (req, res) => {
+router.post('/updateAvatar', async (req, res) => {
   const { avatar } = req.body;
   try {
     await updateUtilisateur(avatar);
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ app.use(cors());
 
 app.use('/get', require('./controllers/route'));
 app.use('/post', require('./controllers/post'));
+app.use('/update', require('./controllers/update'));
 
 app.listen(port, () => {
   console.log(`Le serveur est en cours d'exécution sur le port ${port}`);
